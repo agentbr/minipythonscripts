efbc418140bd8084b06c4de085ab9a938b6a9f42
Bradley! <99224020+agentbr@users.noreply.github.com>
2022-02-23 19:22:08 -0800

    Create daysuntil.py

---
+++ daysuntil.py
@@ -0,0 +1,20 @@
+from datetime import date
+f_date = date.today()
+l_date = date(2022, 4, 16)
+delta = l_date - f_date
+print(delta.days)
+print('Days until baptism')
+
+from datetime import date
+f_date = date.today()
+l_date = date(2022, 1, 1)
+delta = l_date - f_date
+print(delta.days)
+print('Days until birthday')
+
+from datetime import date
+f_date = date.today()
+l_date = date(2022, 12, 25)
+delta = l_date - f_date
+print(delta.days)
+print('Days until Christmas')
